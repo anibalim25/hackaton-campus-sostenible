28900d97b31d53404ea9531539ed9cdcf6cecaf8
Anibal <anibaliglesias04@gmail.com>
2025-05-28 10:31:33 +0200

    backend configurado

---
+++ backend/src/test/java/com/miapp/TestApp.java
@@ -0,0 +1,13 @@
+package com.miapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestApp {
+
+    @Test
+    void ejemploTest() {
+        assertTrue(true);
+    }
+}
